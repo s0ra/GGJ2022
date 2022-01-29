ac7181215e6387d30980426a0851c7967a386f11
feelinggame_keyzhao <feelinggame.keyzhao@gmail.com>
2022-01-29 12:04:45 +0800

    sample scene

--- GGJ2022/Assets/Scripts/Managers/InputManager.cs
+++ GGJ2022/Assets/Scripts/Managers/InputManager.cs
@@ -18,31 +18,36 @@ public class InputManager : MonoBehaviour
         }
     }
 
+
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
     public void InitManager()
     {
 
     }
 
-    private Vector3 screenPoint;
-    private Vector3 offset;
 
-    void OnMouseDown()
-    {
-        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    // void OnMouseDown()
+    // {
+    //     screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    //     offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 
-    }
+    // }
 
-    void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+    // void OnMouseDrag()
+    // {
+    //     Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+    //     Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+    //     transform.position = curPosition;
 
-    }
+    // }
 
 
 }
+
+
 
