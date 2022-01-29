15a0742c86d47e2285fb855e6eec639c37292f0c
Yves <feelinggame.yvesyiu@gmail.com>
2022-01-29 23:06:31 +0800

    update player collider

---
+++ GGJ2022/Assets/Scripts/Actors/EnemyRuntime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRuntime : ActorRuntime
+{
+    public override void FixedUpdateObject()
+    {
+        base.FixedUpdateObject();
+
+        BoxCollider2D forwardChecker = _walkRight ? _rightWallCheckCollider : _leftWallCheckCollider;
+        BoxCollider2D backwardChecker = _walkRight ? _leftWallCheckCollider : _rightWallCheckCollider;
+        if (CheckAnyOverlapCollider(forwardChecker))
+        {
+            //Debug.Log($"forwardChecker:{true}");
+
+            if (!CheckAnyOverlapCollider(backwardChecker))
+            {
+                //Debug.Log($"backwardChecker:{false}");
+
+                Flip(!_walkRight);
+            }
+        }
+
+        TryWalk();
+    }
+}
