5cb9a24111eecf09d4f1885b5d5a54b65bfec4f2
feelinggame_keyzhao <feelinggame.keyzhao@gmail.com>
2022-01-29 17:10:10 +0800

    end point and sce

---
+++ GGJ2022/Assets/Scripts/LevelObjects/EndPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Player"))
+        {
+            GameplayManager.Instance.TryChangeGameState(new GameplayStateData(GameStateId.Victory));
+        }
+    }
+}
